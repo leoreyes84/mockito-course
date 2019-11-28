a1d2e11270cfea6a865adbc16eb0fa86a78a5de1
Leonardo Reyes <Leonardo.ReyesRivero@wnco.com>
2019-11-28 16:42:58 -0500

    Powermockito with static method

--- src/test/java/com/in28minutes/powermock/MockingStaticMethodTest.java
+++ src/test/java/com/in28minutes/powermock/MockingStaticMethodTest.java
@@ -38,7 +38,7 @@ public class MockingStaticMethodTest {
 	Dependency dependency;
 
 	@InjectMocks
-	TodoBusinessImpl todoBusinessImpl;
+	SystemUnderTest systemUnderTest;
 	
 	@Captor
 	ArgumentCaptor<String> stringArgumentCaptor;
@@ -48,83 +48,13 @@ public class MockingStaticMethodTest {
 	@Test
 	public void testRetrieveTodosRelatedToSpring_usingAMock() {
 
-		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+		List<Integer> stats = Arrays.asList(1,2,3);
 
-		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+		when(dependency.retrieveAllStats()).thenReturn(stats);
 
-		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		assertEquals(2, filteredTodos.size());
+		systemUnderTest.methodCallingAStaticMethod();
 	}
 
-	@Test
-	public void testRetrieveTodosRelatedToSpring_withEmptyList() {
-		TodoService todoServiceMock = mock(TodoService.class);
-
-		List<String> todos = Arrays.asList();
-
-		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-
-		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		assertEquals(0, filteredTodos.size());
-	}
-
-	@Test
-	public void testRetrieveTodosRelatedToSpring_usingBDD() {
-		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
-		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
-		// When
-		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		// Then
-		assertThat(filteredTodos.size(), is(2));
-	}
-
-	@Test
-	public void testDeleteTodosNotRelatedToSpring_usingBDD() {
-		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
-		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
-		// When
-		todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
-		// Then (similarities)
-		verify(todoServiceMock, times(1)).deleteTodos("Learn to Dance");
-		verify(todoServiceMock, atLeastOnce()).deleteTodos("Learn to Dance");
-		verify(todoServiceMock, atLeast(1)).deleteTodos("Learn to Dance");
-		then(todoServiceMock).should().deleteTodos("Learn to Dance");
-
-		verify(todoServiceMock, never()).deleteTodos("Learn Spring MVC");
-		then(todoServiceMock).should(never()).deleteTodos("Learn Spring MVC");
-
-		verify(todoServiceMock, never()).deleteTodos("Learn Spring");
-		then(todoServiceMock).should(never()).deleteTodos("Learn Spring");
-	}
-
-	@Test
-	public void testDeleteTodosNotRelatedToSpring_usingBDD_argumentCapture() {
-		
-		// Given
-		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
-		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
-		// When
-		todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
-		// Then (similarities)
-		then(todoServiceMock).should().deleteTodos(stringArgumentCaptor.capture());
-
-		assertThat(stringArgumentCaptor.getValue(), is("Learn to Dance"));
-	}
-
-	@Test
-	public void testDeleteTodosNotRelatedToSpring_usingBDD_argumentCaptureMultipleTimes() {
-		// Declare Argument Captor
-		ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
-
-		List<String> todos = Arrays.asList("Learn to Rock and Roll", "Learn Spring", "Learn to Dance");
-		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
-		// When
-		todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
-		// Then (similarities)
-		then(todoServiceMock).should(times(2)).deleteTodos(stringArgumentCaptor.capture());
-
-		assertThat(stringArgumentCaptor.getAllValues().size(), is(2));
-	}
+	
 
 }
